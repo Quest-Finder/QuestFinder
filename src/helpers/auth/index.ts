e5deee267e0e386a5f044bfc3bd8b82863a118eb
Juliano Sill <sill.juliano@gmail.com>
2024-10-28 23:12:17 -0300

    chore(auth): move helpers to global folder

---
+++ src/helpers/auth/index.ts
@@ -0,0 +1,4 @@
+export { PASSWORD_MIN_LENGTH } from './password-requirements'
+
+export { checkPasswordRequirements } from './check-password-requirements'
+export { getPasswordStatus } from './get-password-status'
